b7a4f60df0fe5bb3a995b6e44450f5cd58820ea5
Artyom Chernetsov <chernetsov.artyom@gmail.com>
2022-05-31 22:07:13 +0700

    WIP: Camel http retry example

--- camel-http-retry-example/src/main/java/ru/archertech/lab/camel/HttpCallGetRoute.java
+++ camel-http-retry-example/src/main/java/ru/archertech/lab/camel/HttpCallGetRoute.java
@@ -7,6 +7,13 @@ public class HttpCallGetRoute extends RouteBuilder {
     public void configure() {
         from("direct:call-get")
                 .routeId("call-get")
+                .log("trying call http")
+//                .process(new Processor() {
+//                    @Override
+//                    public void process(Exchange exchange) throws Exception {
+//                        throw new RuntimeException("test");
+//                    }
+//                })
                 .to("http://localhost:8000/thing/1")
                 .errorHandler(noErrorHandler())
                 .log("${body}");
--- camel-http-retry-example/src/main/java/ru/archertech/lab/camel/HttpRetryRoute.java
+++ camel-http-retry-example/src/main/java/ru/archertech/lab/camel/HttpRetryRoute.java
@@ -1,5 +1,7 @@
 package ru.archertech.lab.camel;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.http.base.HttpOperationFailedException;
 
@@ -11,6 +13,18 @@ public class HttpRetryRoute extends RouteBuilder {
                 .to("direct:call-get")
                 .log("${body}")
                 .onException(HttpOperationFailedException.class)
-                .maximumRedeliveries(2).redeliveryDelay(5000);
+                .maximumRedeliveries(2).redeliveryDelay(2000)
+                .handled(true)
+//                .onException(Throwable.class)
+                .log("got exception test")
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        Exception cause = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
+                        // why cause exception is null ???
+                        System.out.println("last process; " + cause);
+                    }
+                });
+
     }
 }
